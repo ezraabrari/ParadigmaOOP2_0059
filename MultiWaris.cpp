76a0733d5f4ade4575aa42075a10a2f21c026298
ezraabrari <ezraabrari13@gmail.com>
2024-05-27 15:10:50 +0700

    Membuat class Budi yang merujuk pada pekerja dan pelajar

--- MultiWaris.cpp
+++ MultiWaris.cpp
@@ -29,3 +29,13 @@ public:
         }
 };
 
+class budi :public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) {
+            cout << "Budi dibuat\n" << endl;
+        }
+};
