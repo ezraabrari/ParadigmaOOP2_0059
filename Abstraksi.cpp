564a38c6905a2e0cbebb7793c646d9d1f455f0fc
ezraabrari <ezraabrari13@gmail.com>
2024-05-27 15:38:06 +0700

    membuat void setXY

---
+++ Abstraksi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Abstraksiklas {
+private: string x, y;
+
+public:
+
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+}
